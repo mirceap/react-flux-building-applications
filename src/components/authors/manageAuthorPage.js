f38130144bd6a3d4123b63261d91ab381d864860
mirceap <mirceap@student.42.fr>
2018-10-26 01:10:38 +0300

    08 - React Forms - after 07 Programmatic Redirects

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,9 +1,15 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
+var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+	mixins: [
+		Router.Navigation
+	],
+
 	getInitialState: function() {
 		return {
 			author: { id: '', firstName: '', lastName: '' }
@@ -17,12 +23,20 @@ var ManageAuthorPage = React.createClass({
 			return this.setState({author: this.state.author});
 	},
 
+	saveAuthor: function(event) {
+		event.preventDefault();
+		AuthorApi.saveAuthor(this.state.author);
+		this.transitionTo('authors');
+
+	},
+
 
 	render: function() {
 		return (
 				<AuthorForm 
 					author={this.state.author}
-					onChange={this.setAuthorState} />
+					onChange={this.setAuthorState} 
+					onSave={this.saveAuthor} />
 		);
 	}
 });
