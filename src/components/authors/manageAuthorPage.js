896684fe6315909d94d1030ae47d1ac214cf0f0d
mirceap <mirceap@student.42.fr>
2018-10-26 23:12:17 +0300

    08 - Routes - after 12 Form Hydration via URL

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -11,18 +11,36 @@ var ManageAuthorPage = React.createClass({
 		Router.Navigation
 	],
 
+	statics: {
+		willTransitionFrom: function(transition, component) {
+			if (component.state.dirty && !confirm('Leave without savings?')){
+				transition.abort();
+			}
+		}
+	},
+
 	getInitialState: function() {
 		return {
 			author: { id: '', firstName: '', lastName: '' },
-			errors: {}
+			errors: {},
+			dirty: false
 		};
 	},
 
+	componentWillMount: function(){
+		var authorId = this.props.params.id; // from the path "/author:id"
+
+		if (authorId) {
+			this.setState({author: AuthorApi.getAuthorById(authorId)});
+		}
+	},
+
 	setAuthorState: function(event) {
-			var filed = event.target.name;
-			var value = event.target.value;
-			this.state.author[filed] = value;
-			return this.setState({author: this.state.author});
+		this.setState({dirty: true});
+		var filed = event.target.name;
+		var value = event.target.value;
+		this.state.author[filed] = value;
+		return this.setState({author: this.state.author});
 	},
 
 	authorFormIsValid: function() {
@@ -51,6 +69,7 @@ var ManageAuthorPage = React.createClass({
 			return;
 		}
 		AuthorApi.saveAuthor(this.state.author);
+		this.setState({dirty: false});
 		toastr.success("Author saved.");
 		this.transitionTo('authors');
 
