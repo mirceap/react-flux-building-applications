64d9a9652976abf5d40ee63da7a1e283ba934534
mirceap <mirceap@student.42.fr>
2018-11-13 00:19:13 +0200

    10 Flux Demos - after 06 Stores: Interactions
    10 Flux Demos - after 06 Stores: Interactions- bug fixes
    
    10 Flux Demos - after 06 Stores: Interactions- bug fixes 2

--- src/stores/authorStore.js
+++ src/stores/authorStore.js
@@ -3,7 +3,7 @@
 var Dispatcher = require('../dispatcher/appDispatcher');
 var ActionTypes = require('../constants/actionTypes');
 var EventEmitter = require('events').EventEmitter;
-var assign = require('object-assgin');
+var assign = require('object-assign');
 var _ = require('lodash');
 var CHANGE_EVENT = 'change';
 
@@ -12,15 +12,15 @@ var _authors = [];
 var AuthorStore = assign({}, EventEmitter.prototype, {
 	
 	addChangeListener: function(callback) {
-	 	this.on(CHANGE_EVENT, callback);
+		this.on(CHANGE_EVENT, callback);
 	},
 
 	removeChangeListener: function(callback) {
-	 	this.removeListener(CHANGE_EVENT, callback);
+		this.removeListener(CHANGE_EVENT, callback);
 	},
 
 	emitChange: function() {
-	 	this.emit(CHANGE_EVENT);
+		this.emit(CHANGE_EVENT);
 	},
 
 	getAllAuthors: function(){
@@ -28,7 +28,7 @@ var AuthorStore = assign({}, EventEmitter.prototype, {
 	},
 
 	getAuthorById: function(id){
-		return _.find(_authors, {id : id});
+		return _.find(_authors, {id: id});
 	}
 
 });
